126d435fe1ebd59e5d83c89a835095ce65569139
Maxi-SR <schmidtmaxi46@gmail.com>
2022-12-23 08:57:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $('.carousel').slick({
+            slidesToShow: 1,
+            dots: false,
+            centerMode: false,
+        });
+    });
+});
