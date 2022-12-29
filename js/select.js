a1af5e8dbd28b953524b75b79372579fd3aa705c
Maxi-SR <schmidtmaxi46@gmail.com>
2022-12-29 17:34:07 -0300

    send data to a separate file

--- js/select.js
+++ js/select.js
@@ -1,10 +1,3 @@
-var dptosLocs = {
-    "Artigas": ["Artigas", "Bella Unión"],
-    "Canelones": ["Canelones", "Santa Lucía"],
-    "Montevideo": ["Montevideo"],
-    "Salto": ["Salto", "Daymán", "Arapey"]
-};
-
 const selectDepto = document.getElementById("depto");
 const selectLoc = document.getElementById("loc");
 
