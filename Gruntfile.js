8177cd0c8024c3ce52f0e750367accf9bf5a8573
Maxi-SR <schmidtmaxi46@gmail.com>
2022-12-30 18:27:17 -0300

    grunt css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,19 @@ module.exports = function (grunt) {
 
     uglify: {
       build: {
-        src: ['src/*.js', 'src/validations/*.js', 'data/*.js'],
-        dest: 'js/scripts.min.js'
+        src: ['src/*.js', 'src/validations/*.js'],
+        dest: 'dist/app.min.js'
       }
     },
 
+    cssmin: {
+      build: {
+        src: ['assets/*.css'],
+        dest: 'dist/app.min.css'
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
 };
